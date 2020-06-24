f49dfaf08b8fa3e25b58f8422c19ae73dc869298
Whitney Kidd <whitneymkidd@gmail.com>
2020-06-24 10:27:39 -0600

    update routes for merchants and items

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "/", to: "welcome#index"
-  
+
   resources :merchants
   # get "/merchants", to: "merchants#index"
   # get "/merchants/new", to: "merchants#new"
@@ -10,22 +10,25 @@ Rails.application.routes.draw do
   # get "/merchants/:id/edit", to: "merchants#edit"
   # patch "/merchants/:id", to: "merchants#update"
   # delete "/merchants/:id", to: "merchants#destroy"
-
-  get "/items", to: "items#index"
-  get "/items/:id", to: "items#show"
-  get "/items/:id/edit", to: "items#edit"
-  patch "/items/:id", to: "items#update"
+  resources :items do
+    resources :reviews
+  end
+  # get "/items", to: "items#index"
+  # get "/items/:id", to: "items#show"
+  # get "/items/:id/edit", to: "items#edit"
+  # patch "/items/:id", to: "items#update"
   get "/merchants/:merchant_id/items", to: "items#index"
   get "/merchants/:merchant_id/items/new", to: "items#new"
   post "/merchants/:merchant_id/items", to: "items#create"
-  delete "/items/:id", to: "items#destroy"
+  # delete "/items/:id", to: "items#destroy"
 
-  get "/items/:item_id/reviews/new", to: "reviews#new"
-  post "/items/:item_id/reviews", to: "reviews#create"
+  # get "/items/:item_id/reviews/new", to: "reviews#new"
+  # post "/items/:item_id/reviews", to: "reviews#create"
 
-  get "/reviews/:id/edit", to: "reviews#edit"
-  patch "/reviews/:id", to: "reviews#update"
-  delete "/reviews/:id", to: "reviews#destroy"
+  resources :reviews
+  # get "/reviews/:id/edit", to: "reviews#edit"
+  # patch "/reviews/:id", to: "reviews#update"
+  # delete "/reviews/:id", to: "reviews#destroy"
 
   post "/cart/:item_id", to: "cart#add_item"
   get "/cart", to: "cart#show"
@@ -97,14 +100,6 @@ Rails.application.routes.draw do
     get "/users/:user_id", to: "users#show"
 
   end
-
-  # namespace :admin do
-  #   root "dashboard#show"
-  #   get "/dashboard", to: "dashboard#show"
-  #   resources :orders, only: [:update]
-  #   resources :users, only: [:index, :show]
-  #   resources :merchants, only: [:index, :show, :update, :destroy]
-  # end
 
   resources :logout, only: [:index]
 end
